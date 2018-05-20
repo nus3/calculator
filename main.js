e9d1b7c532e91989f7ea31cc73eadd30fc3f6f09
yhada <yhada@techloco.co.jp>
2018-05-20 17:34:59 +0900

    正負の変更実装

--- main.js
+++ main.js
@@ -14,15 +14,19 @@ class Calculator
         _calcuGlobalObj.number = ''
         _calcuGlobalObj.Operators = []
 
-        Calculator.setNumber(0)
+        Calculator.setInputedNumber(0)
     }
 
-    static setNumber(number) {
+    static setInputedNumber(number) {
         document.getElementById('calculator__value').innerText = number
     }
 
     static setTotalNumber() {
         _calcuGlobalObj.totalNumber = _calcuGlobalObj.number
+    }
+
+    static setNumber(number) {
+        _calcuGlobalObj.number = number
     }
 
     pushNumber(inputedNumber) {
@@ -32,15 +36,28 @@ class Calculator
         if (_calcuGlobalObj.number.length < 9) {
             _calcuGlobalObj.number += inputedNumber
         }
-        Calculator.setNumber(_calcuGlobalObj.number)
+        Calculator.setInputedNumber(_calcuGlobalObj.number)
     }
 
     percentNumber() {
         if (_calcuGlobalObj.number) {
-            _calcuGlobalObj.number *= 0.01           
+            _calcuGlobalObj.number *= 0.01
+            Calculator.setInputedNumber(_calcuGlobalObj.number)
+            Calculator.setTotalNumber()
+        }        
+    }
+
+    changeSign() {
+        if (_calcuGlobalObj.number) {
+            if (_calcuGlobalObj.number.indexOf('-') == 0) {
+                _calcuGlobalObj.number = _calcuGlobalObj.number.replace('-', '')    
+            }else {
+                _calcuGlobalObj.number = `-${_calcuGlobalObj.number}`
+            }
+
+            Calculator.setInputedNumber(_calcuGlobalObj.number)
+            Calculator.setNumber(_calcuGlobalObj.number)
         }
-        Calculator.setNumber(_calcuGlobalObj.number)
-        Calculator.setTotalNumber()        
     }
 }
 
@@ -56,6 +73,11 @@ const pushNumberInputed = () => {
 const percentNumberInputed = () => {
     const calcu = new Calculator()
     calcu.percentNumber()
+}
+
+const changeSignInputed = () => {
+    const calcu = new Calculator()
+    calcu.changeSign()
 }
 
 
